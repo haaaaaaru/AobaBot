b10e727a5a2cd2fc5d83afabe4446a7489c64feb
Haru301 <39460338+seungjin1302@users.noreply.github.com>
2020-03-25 14:08:32 +0900

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -7,7 +7,8 @@ public class Bot
 {
     public static void main(String args[]) throws Exception
     {
-        JDA builder = new JDABuilder("NjkyMjA2NDMwNjc3MTA2NzM5.Xnrjxw.YY9dTI6fpoiXkcR-gGf99XG_fWk").build();
+        JDA builder = new JDABuilder("NjkyMjA2NDMwNjc3MTA2NzM5.Xnrjxw.YY9dTI6fpoiXkcR-gGf99XG_fWk").build(); 
+        //저 token을 자기 봇 토큰으로 변경할것
 
         builder.addEventListener(new HelloEvent());
     }
